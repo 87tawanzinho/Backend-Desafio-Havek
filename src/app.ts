28b505e8cc3610c6fcfb4c73e836d7601f1d1fec
tawan <87tawanzinho@gmail.com>
2023-12-28 18:53:14 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import mongooseConnection from "./connectDB/mongooseConnection";
 import router from "./routes";
-
+var cors = require("cors");
 require("dotenv").config();
 mongooseConnection();
 
@@ -9,5 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors);
 
 app.listen(4000, () => console.log("Sucess"));
