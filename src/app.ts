909c06b2169abcd7f35a4411ef218d84398b8224
tawan <87tawanzinho@gmail.com>
2023-12-29 00:01:29 -0300

    how

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(router);
 
 app.listen(4000, () => console.log("Sucess"));
