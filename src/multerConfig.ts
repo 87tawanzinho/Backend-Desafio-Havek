7a48034564d7779355a6f64f9b637bab570d6a6f
tawan <87tawanzinho@gmail.com>
2023-12-28 21:35:22 -0300

    hahaha

--- src/multerConfig.ts
+++ src/multerConfig.ts
@@ -1,8 +1,7 @@
 import multer from "multer";
-import path from "path";
 export const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, path.resolve("uploads")); // gera o path da maneira correta
+    callback(null, `${__dirname}/public`);
   },
   filename: (req, file, callback) => {
     const time = new Date().getTime();
